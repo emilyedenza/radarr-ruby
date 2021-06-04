57574db72bf110e74438d704011a8bce95fa2c1a
Dylan Boyd <5061312+dylanjboyd@users.noreply.github.com>
2021-06-04 15:48:00 +1200

    Implement remaining Zarr API calls

--- api/zarr_api.rb
+++ api/zarr_api.rb
@@ -5,6 +5,7 @@ require 'faraday_middleware'
 
 ##
 # Makes requests to Sonarr and Radarr APIs (as operations between the two are interchangeable).
+# The "Z" in Zarr is a placeholder to indicate that it works across both Sonarr and Radarr.
 class ZarrApi
   def initialize(zarr_config)
     @conn = Faraday.new(url: zarr_config.base, params: { apikey: zarr_config.api_key }) do |f|
@@ -16,19 +17,34 @@ class ZarrApi
     end
   end
 
+  ##
+  # Delete a single queue item by ID.
+  # If +should_blacklist+ is true, this will prevent the API from fetching this release in the future.
   def delete_queue_item(item_id, should_blacklist: true)
     @conn.delete("queue/#{item_id}", { should_blacklist: should_blacklist })
   end
 
+  ##
+  # Query all movie/show downloads.
   def queue
     @conn.get('queue').body
   end
 
+  ##
+  # Query API for free disk space.
   def free_disk_space
     @conn.get('diskspace').body
   end
 
-  def commands; end
+  ##
+  # Query currently running commands.
+  def commands
+    @conn.get('command').body
+  end
 
-  def restart; end
+  ##
+  # Restart the whole Zarr application.
+  def restart
+    @conn.post('system/restart').body
+  end
 end
